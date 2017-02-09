b259a0fe5c67d3facc1eed0010102ac6b00952d9
bl00m <leon.pierre@openmailbox.org>
2017-02-09 17:15:59 +0100

    Detect winner

--- js/script.js
+++ js/script.js
@@ -28,6 +28,11 @@ function update() {
 
                 boxes[j].appendChild(jeton);
 
+                var winner = checkWinner();
+                if (winner != 0) {
+                    alert('Le joueur ' + winner + ' à gagné !');
+                }
+
                 if (joueur == 1) {
                     joueur = 2;
                 } else {
@@ -101,14 +106,96 @@ function printTab() {
 
 function checkWinner() {
     for (var i = 0; i < tableau.length; i++) {
-
         for (var j = 0; j < tableau[0].length; j++) {
             if (tableau[i][j] != 0) {
                 var couleur = tableau[i][j];
+
+                iNext = i;
+                jNext = j;
+
+                while (iNext < tableau.length && tableau[iNext][j] == couleur) {
+                    iNext++;
+                }
+
+                if (iNext - i == 4) {
+                    return joueur;
+                }
+                iNext = i;
+
+
+                while (iNext > -1 && tableau[iNext][j] == couleur) {
+                    iNext--;
+                }
+
+                if (i - iNext == 4) {
+                    return joueur;
+                }
+                iNext = i;
+
+                while (jNext < tableau[0].length && tableau[i][jNext] == couleur) {
+                    jNext++;
+                }
+
+                if (j - jNext == 4) {
+                    return joueur;
+                }
+                jNext = j;
+
+                while (jNext > -1 && tableau[i][jNext] == couleur) {
+                    jNext++;
+                }
+
+                if (jNext - j == 4) {
+                    return joueur;
+                }
+                jNext = j;
+
+                while (iNext < tableau.length && jNext < tableau[0].length && tableau[iNext][jNext] == couleur) {
+                    iNext++;
+                    jNext++;
+                }
+
+                if (iNext - i == 4 && jNext - j == 4) {
+                    return joueur;
+                }
+                iNext = i;
+                jNext = j;
+
+                while (iNext < tableau.length && jNext > -1 && tableau[iNext][jNext] == couleur) {
+                    iNext++;
+                    jNext--;
+                }
+
+                if (iNext - i == 4 && j - jNext == 4) {
+                    return joueur;
+                }
+                iNext = i;
+                jNext = j;
+
+                while (iNext > -1 && jNext < tableau[0].length && tableau[iNext][jNext] == couleur) {
+                    iNext--;
+                    jNext++;
+                }
+
+                if (i - iNext == 4 && jNext - j == 4) {
+                    return joueur;
+                }
+                iNext = i;
+                jNext = j;
+
+                while (iNext > -1 && jNext > -1 && tableau[iNext][jNext] == couleur) {
+                    iNext--;
+                    jNext--;
+                }
+
+                if (i - iNext == 4 && j - jNext == 4) {
+                    return joueur;
+                }
+                iNext = i;
+                jNext = j;
+
             }
-
         }
-
     }
-
+    return 0;
 }
